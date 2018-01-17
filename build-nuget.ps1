124c85186141431266f677ddf4799707ab777f84
schwamster <bastian.toepfer@gmail.com>
2018-01-17 20:45:38 +0100

    small changes to ealier merged pr pushed new package with version 1.0.1

--- build-nuget.ps1
+++ build-nuget.ps1
@@ -1,3 +1,4 @@
+rmdir -Force -Recurse .\package
 dotnet build -c Release
 dotnet pack -c Release -o ../../package
 dotnet nuget push .\package\*.nupkg --source https://www.nuget.org/api/v2/package
--- src/ParameterStoreConfigurationProvider/ParameterStoreConfigurationProvider.cs
+++ src/ParameterStoreConfigurationProvider/ParameterStoreConfigurationProvider.cs
@@ -2,6 +2,7 @@
 using Amazon.SimpleSystemsManagement.Model;
 using Microsoft.Extensions.Configuration;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace ParameterStoreConfigurationProvider
 {
@@ -17,40 +18,45 @@ namespace ParameterStoreConfigurationProvider
 
         public override void Load()
         {
+            GetParametersRequest request = PrepareRequest();
+
+            GetParametersResponse response;
+
             if (this.configurationSource.UseDefaultCredentials)
             {
                 using (var client = new AmazonSimpleSystemsManagementClient(Amazon.RegionEndpoint.GetBySystemName(this.configurationSource.Region)))
                 {
-                    var request = new GetParametersRequest()
-                    {
-                        Names = this.configurationSource.ParameterMapping.Select(x => x.AwsName).ToList(),
-                        WithDecryption = this.configurationSource.WithDecryption
-                    };
 
-                    var response = client.GetParametersAsync(request).Result;
 
-                    this.Data = response.Parameters.ToDictionary(
-                        x => this.configurationSource.ParameterMapping.First(pm => pm.AwsName == x.Name).SettingName,
-                        x => x.Value);
+                    response = client.GetParametersAsync(request).Result;
                 }
             }
             else
             {
                 using (var client = new AmazonSimpleSystemsManagementClient(this.configurationSource.AwsCredential, Amazon.RegionEndpoint.GetBySystemName(this.configurationSource.Region)))
                 {
-                    var request = new GetParametersRequest()
-                    {
-                        Names = this.configurationSource.ParameterMapping.Select(x => x.AwsName).ToList(),
-                        WithDecryption = this.configurationSource.WithDecryption
-                    };
 
-                    var response = client.GetParametersAsync(request).Result;
-
-                    this.Data = response.Parameters.ToDictionary(
-                        x => this.configurationSource.ParameterMapping.First(pm => pm.AwsName == x.Name).SettingName,
-                        x => x.Value);
+                    response = client.GetParametersAsync(request).Result;
                 }
             }
+
+            MapResult(response);
+        }
+
+        internal GetParametersRequest PrepareRequest()
+        {
+            return new GetParametersRequest()
+            {
+                Names = this.configurationSource.ParameterMapping.Select(x => x.AwsName).ToList(),
+                WithDecryption = this.configurationSource.WithDecryption
+            };
+        }
+
+        internal void MapResult(GetParametersResponse response)
+        {
+            this.Data = response.Parameters.ToDictionary(
+                                    x => this.configurationSource.ParameterMapping.First(pm => pm.AwsName == x.Name).SettingName,
+                                    x => x.Value);
         }
     }
 }
