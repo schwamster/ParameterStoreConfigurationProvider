b795831327ed87fee9f3e9965a49856e9fc0b4ac
Sergio <sergio@Madelenes-MacBook-Pro.local>
2018-01-18 19:48:35 +0100

    small improvements and checking for validity of the parameter request

--- src/ParameterStoreConfigurationProvider/ParameterStoreConfigurationProvider.cs
+++ src/ParameterStoreConfigurationProvider/ParameterStoreConfigurationProvider.cs
@@ -2,6 +2,7 @@
 using Amazon.SimpleSystemsManagement.Model;
 using Microsoft.Extensions.Configuration;
 using System.Linq;
+using System;
 
 namespace ParameterStoreConfigurationProvider
 {
@@ -15,40 +16,55 @@ namespace ParameterStoreConfigurationProvider
             this.configurationSource = configurationSource;
         }
 
+        private void CheckParametersValidity(GetParametersResponse response)
+        {
+            if (response.InvalidParameters.Count > 0)
+            {
+                String wrongParams = "";
+                foreach (var item in response.InvalidParameters)
+                {
+                    wrongParams = item + " ";
+                }
+
+                throw new Exception("You have requested invalid parameters " + wrongParams);
+            }
+        }
+
+        private void MappingClientResponseToData(AmazonSimpleSystemsManagementClient client)
+        {
+            var request = new GetParametersRequest()
+            {
+                Names = this.configurationSource.ParameterMapping.Select(x => x.AwsName).ToList(),
+                WithDecryption = this.configurationSource.WithDecryption
+            };
+            try
+            {
+                var response = client.GetParametersAsync(request).Result;
+                CheckParametersValidity(response);
+                this.Data = response.Parameters.ToDictionary(
+                    x => this.configurationSource.ParameterMapping.First(pm => pm.AwsName == x.Name).SettingName,
+                    x => x.Value);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
         public override void Load()
         {
             if (this.configurationSource.UseDefaultCredentials)
             {
                 using (var client = new AmazonSimpleSystemsManagementClient(Amazon.RegionEndpoint.GetBySystemName(this.configurationSource.Region)))
                 {
-                    var request = new GetParametersRequest()
-                    {
-                        Names = this.configurationSource.ParameterMapping.Select(x => x.AwsName).ToList(),
-                        WithDecryption = this.configurationSource.WithDecryption
-                    };
-
-                    var response = client.GetParametersAsync(request).Result;
-
-                    this.Data = response.Parameters.ToDictionary(
-                        x => this.configurationSource.ParameterMapping.First(pm => pm.AwsName == x.Name).SettingName,
-                        x => x.Value);
+                    MappingClientResponseToData(client);
                 }
             }
             else
             {
                 using (var client = new AmazonSimpleSystemsManagementClient(this.configurationSource.AwsCredential, Amazon.RegionEndpoint.GetBySystemName(this.configurationSource.Region)))
                 {
-                    var request = new GetParametersRequest()
-                    {
-                        Names = this.configurationSource.ParameterMapping.Select(x => x.AwsName).ToList(),
-                        WithDecryption = this.configurationSource.WithDecryption
-                    };
-
-                    var response = client.GetParametersAsync(request).Result;
-
-                    this.Data = response.Parameters.ToDictionary(
-                        x => this.configurationSource.ParameterMapping.First(pm => pm.AwsName == x.Name).SettingName,
-                        x => x.Value);
+                    MappingClientResponseToData(client);
                 }
             }
         }
